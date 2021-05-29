b2c6d1f5002b98454dbad10b9ba18b25f8284a49
Amit Pal <ampal@adobe.com>
2021-05-30 00:04:08 +0530

    first commit

---
+++ src/main/java/Maven_first/demo1.java
@@ -0,0 +1,5 @@
+package Maven_first;
+
+public class demo1 {
+
+}
---
+++ src/test/java/demo.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class demo {
+	
+	@Test
+	public void API()
+	{
+		System.out.println("APIqqqqqqq");
+		
+	}
+	@Test
+	public void Api1()
+	{
+		System.out.println("APIqqqqqqqqqqqqqqqq1");
+		
+	}
+
+}
